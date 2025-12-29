9d625f77dc6a824e867dbb6e51ac9d7bf856f839
Starcross-Trading-Ltd <bearsamb1@gmail.com>
2025-12-29 17:50:19 +0000

    Update App.tsx to add xmas in evelina 2025

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ServicesPage from './pages/services'
 import ContactPage from './pages/contact'
 import ComplimentsPage from './pages/compliments'
 import NewsPage from './pages/news'
+import EvelinaXmasNewsPage from './pages/news/xmasinevelina'
 import HaberdashersNewsPage from './pages/news/haberdashers'
 import Pulse6NewsPage from './pages/news/pulse-6'
 import Pulse5NewsPage from './pages/news/pulse-5'
@@ -77,6 +78,7 @@ function App() {
               <Route path="/compliments" element={<ComplimentsPage />} />
               <Route path="/news/haberdashers" element={<HaberdashersNewsPage />} />
               <Route path="/news" element={<NewsPage />} />
+              <Route path="/news/xmas-in-evelina" element={<EvelinaXmasNewsPage />} />
               <Route path="/news/pulse-6" element={<Pulse6NewsPage />} />
               <Route path="/news/pulse-5" element={<Pulse5NewsPage />} />
               <Route path="/news/kingussieschool" element={<KingussieNewsPage />} />
